aefacb058a54c5855a05341f297508f1cf428276
James McDonagh <jameskwm@protonmail.com>
2024-04-19 21:03:27 -0400

    not sure why this didn't get added

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+document.getElementById('toggleButton').addEventListener('click', function() {
+    var sidebar = document.getElementById('sidebar');
+    var main = document.getElementById('main');
+    if (sidebar.style.width === '250px') {
+        sidebar.style.width = '0';
+        main.style.marginLeft = '0';
+    } else {
+        sidebar.style.width = '250px';
+        main.style.marginLeft = '250px';
+    }
+});

e75cd02841a93e8ac779fc21019de84475350b5c
James McDonagh <jameskwm@protonmail.com>
2024-04-20 22:09:31 -0400

    this is gonna be hard

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 document.getElementById('toggleButton').addEventListener('click', function() {
     var sidebar = document.getElementById('sidebar');
     var main = document.getElementById('main');
-    if (sidebar.style.width === '250px') {
-        sidebar.style.width = '0';
-        main.style.marginLeft = '0';
-    } else {
-        sidebar.style.width = '250px';
-        main.style.marginLeft = '250px';
-    }
+    sidebar.classList.toggle('open');
+    main.classList.toggle('shifted');
 });
+
+function navigateTo(page) {
+    window.location.href = page; // Navigate to the specified URL
+}
